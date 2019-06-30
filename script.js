4e95923803b61df48f9987d2aaae3ed853a011cb
Hamster726 <114generals>
2019-06-30 03:04:55 +0300

    find-me-a-movie v0.3
    Что нового:
    - Новый дизайн
    - Новый вывод результатов
    - Добавлена функция "Подробнее" при нажатии на постер
    - Обновлены обработчики событий:
         1. При загрузке отображается спиннер вместо слова "Звгрузка"
         2. При попытке ввести пустую строку выпадает предупреждение
         3. Если ничего не найдено, выпадает информация об этом событии

--- script.js
+++ script.js
@@ -6,57 +6,159 @@ function apiSearch(event) {
 
     event.preventDefault();                                     //не обновлять страницу при нажатии энтер в строке
     const searchText = document.querySelector('.form-control').value;
+    if (searchText.trim().length === 0) {
+        movie.innerHTML = '<h2 class="col-12 text-center text-danger">Введите запрос</h2>';
+        return;
+    }
     const server = 'https://api.themoviedb.org/3/search/multi?api_key=6aab1502d97ac3acab566ecf831eae0d&language=ru&query=' + searchText;
     const urlPoster = 'https://image.tmdb.org/t/p/original';
-    movie.innerHTML = 'Загрузка';
+
+    movie.innerHTML = '<div class="spinner"></div>';
 
     fetch(server)
         .then(function (value) {
-            if (value.status !== 200){
+            if (value.status !== 200) {
                 return Promise.reject(value);
             }
-           return value.json();
+            return value.json();
         })
         .then(function (output) {
             let inner = '';
-
+            if (output.results.length === 0) {
+                inner += '<h2 class="col-12 text-center text-info">Ничего не найдено</h2>';
+            }
             output.results.forEach(function (item) {
-                //console.log(item);
                 let nameItem = item.name || item.title;
                 let releaseDate = item.first_air_date || item.release_date;
-                let picPath = item.poster_path;
+                let picPath = item.poster_path; //urlPoster + picPath
+                const poster = picPath ? urlPoster + picPath : './img/no-poster.jpg';
                 let genre = item.media_type;
                 let description = item.overview;
-                let vote = item.vote_average;
+                let votes = item.vote_average;
 
-                inner += `<h4 class='text-uppercase h4 mx-auto'>${nameItem}</h4>`;
+                let dataInfo = '';
+
+                if (item.media_type !== 'person') {
+                    dataInfo = `data-id="${item.id}" data-type="${item.media_type}"`;
+                }
+
                 inner += `
-             <table class="table table-borderless">
-                 <tbody>
-                     <tr>
-                         <td>Дата выхода: ${releaseDate}</td>
-
-                     </tr>
-                     <tr>
-                         <td>Жанр: ${genre}</td>
-                     </tr>
-                     <tr>
-                          <td><img src="${urlPoster  + picPath}" width="300px"></td>
-                          <td class="align-middle">${description}</td>
-                     </tr>
-                     <tr>
-                          <td>Рейтинг: ${vote} из 10</td>
-                     </tr>
-                 </tbody>
-             </table>`;
+<div class="col-12 col-md-4 col-xl-3 text-center item">
+    <img src= "${poster}" alt="${nameItem}" ${dataInfo} class="mw-100 poster">
+    <h5>${nameItem}</h5>
+</div>`;
             });
-
             movie.innerHTML = inner;
+
+            addEveneMedia();
         })
         .catch(function (reason) {
-            movie.innerHTML = 'Ой, что-то пошло не так :('
+            movie.innerHTML = '<h2 class="col-12 text-center text-danger">Ой, что-то пошло не так :(</h2>';
+            console.error('error: ' + reason);
+        });
+
+}
+
+searchForm.addEventListener('submit', apiSearch);
+
+function addEveneMedia() {
+    const media = movie.querySelectorAll('img[data-id]');
+    media.forEach(function (elem) {
+        elem.style.cursor = 'pointer';
+        elem.addEventListener('click', showFullInfo);
+    });
+
+}
+
+function showFullInfo() {
+    let url = '';
+    if (this.dataset.type === 'movie') { // dataset обрабатывает бразуер, все атрибуты с data-* скидывает в обьекты
+        url = 'https://api.themoviedb.org/3/movie/' + this.dataset.id + '?api_key=6aab1502d97ac3acab566ecf831eae0d&language=ru';
+    } else if (this.dataset.type === 'tv') {
+        url = 'https://api.themoviedb.org/3/tv/' + this.dataset.id + '?api_key=6aab1502d97ac3acab566ecf831eae0d&language=ru';
+    } else {
+        movie.innerHTML = '<h2 class="col-12 text-center text-danger">Произошла ошибка, повторите позже</h2>';
+    }
+
+    fetch(url)
+        .then(function (value) {
+            if (value.status !== 200) {
+                return Promise.reject(value);
+            }
+            return value.json();
+        })
+        .then(function (output) {
+            const urlPoster = 'https://image.tmdb.org/t/p/original';
+            let poster = output.poster_path ? urlPoster + output.poster_path : './img/no-poster.jpg';
+            let name = output.name || output.title;
+            let releaseDate = output.first_air_date || output.release_date;
+            let type = output.media_type === 'movie' ? 'Фильм' : 'Сериал';
+            let description = output.overview ? output.overview : 'Отсутствует';
+            let votes = output.vote_average ? output.vote_average : 'Неизвестно';
+            const background = document.querySelector('#container');
+            background.style.backgroundImage = `url("${poster}")`;
+            movie.innerHTML = `
+<h4 class="col-12 text-center text-info">
+${name}
+</h4> 
+
+<div class="col-4 text-center">
+
+<img src="${poster}" alt="${name}" class="mw-100 poster">
+${output.homepage ? `<p><a href="${output.homepage}" target="_blank">Официальная страница</a></p>` : ''}
+${output.imdb_id ? `<p><a href="https://www.imdb.com/title/${output.imdb_id}" target="_blank">Страница IMDb.com</a></p>` : ''}
+</div>
+
+<div class="col-8">
+<p>Премьера: ${releaseDate}</p>
+<p>Тип: ${type}</p>
+<p>Рейтинг: ${votes}</p>
+<hr>
+<h6>Обзор</h6>
+<p>${description}</p>
+</div>`;
+        })
+        .catch(function (reason) {
+            movie.innerHTML = '<h2 class="col-12 text-center text-danger">Ой, что-то пошло не так :(</h2>';
             console.error('error: ' + reason);
         });
 }
 
-searchForm.addEventListener('submit', apiSearch);
+document.addEventListener('DOMContentLoaded', function () {
+    const urlPoster = 'https://image.tmdb.org/t/p/original';
+
+    fetch('https://api.themoviedb.org/3/trending/all/week?api_key=6aab1502d97ac3acab566ecf831eae0d&language=ru')
+        .then(function (value) {
+            if (value.status !== 200) {
+                return Promise.reject(value);
+            }
+            return value.json();
+        })
+        .then(function (output) {
+            let inner = '<h4 class="col-12 text-center text-info">Тренды недели</h4>';
+            if (output.results.length === 0) {
+                inner += '<h2 class="col-12 text-center text-info">Ничего не найдено</h2> ';
+            }
+            output.results.forEach(function (item) {
+                let nameItem = item.name || item.title;
+                let mediaType = item.title ? 'movie' : 'tv'; //Некий костыль
+                let picPath = item.poster_path; //urlPoster + picPath
+                const poster = picPath ? urlPoster + picPath : './img/no-poster.jpg';
+
+                let dataInfo = `data-id="${item.id}" data-type="${mediaType}"`;
+
+                inner += `
+<div class="col-12 col-md-4 col-xl-3 text-center item">
+    <img src= "${poster}" alt="${nameItem}" ${dataInfo} class="mw-100 poster">
+    <h5>${nameItem}</h5>
+</div>`;
+            });
+            movie.innerHTML = inner;
+
+            addEveneMedia();
+        })
+        .catch(function (reason) {
+            movie.innerHTML = '<h2 class="col-12 text-center text-danger">Ой, что-то пошло не так :(</h2>';
+            console.error('error: ' + reason);
+        });
+});
